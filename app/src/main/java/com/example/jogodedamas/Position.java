c4ebc7f6d551ae3641201b2d3161e73d28969ecb
CidyWallace <cidywallace12@gmail.com>
2022-01-18 12:52:13 -0300

    Peça nas casas pretas adicionado

--- app/src/main/java/com/example/jogodedamas/Position.java
+++ app/src/main/java/com/example/jogodedamas/Position.java
@@ -1,10 +1,10 @@
 package com.example.jogodedamas;
 
-public class Posicao {
+public class Position {
     private int x;
     private int y;
 
-    public Posicao(int x, int y){
+    public Position(int x, int y){
         this.x = x;
         this.y = y;
     }
